d235221f44237d0f636e6d2fdf919a85f8939dce
Xiaoxing Cheng <hello@chengxiaoxing.me>
2023-07-25 18:20:25 +0800

    initial commit for qt

--- configurations/default6.sh
+++ configurations/default6.sh
@@ -2,6 +2,8 @@
 # This is a template script to install the external project
 # You should create a configuration folder and copy this script
 # to the folder for actual installation.
+
+version=${1:-v6.3.0}
 config=$(basename "${BASH_SOURCE[0]}" .sh)
 script_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd)"
 root="$script_dir/.."
@@ -11,3 +13,14 @@ install_dir="$script_dir/../install/$config"
 
 cmake -S $source_dir -B $build_dir -DCMAKE_INSTALL_PREFIX=$install_dir -G Ninja
 cmake --build $build_dir --target install
+
+cd $root
+git clone https://code.qt.io/qt/qt5.git source
+cd $source_dir
+git checkout $version
+./init-repository --module-subset="qtbase"
+mkdir -p $build_dir
+cd $build_dir
+${source_dir}/configure -prefix ${install_dir} -release -opensource -confirm-license -nomake tools -nomake examples -nomake tests
+cmake --build . --parallel 4
+cmake --install .
